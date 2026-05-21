e9bd009319aa5fddda611f3deb23d581dd655456
djaceplace-coder <djaceplace@gmail.com>
2026-05-21 09:55:22 +0100

    Update ​vite.config.ts

--- ​vite.config.ts
+++ ​vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative assets paths for GitHub Pages
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: './'
 });
-
